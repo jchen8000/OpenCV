abc8eea6278ef5d0ec5dfd7706bb8e9b6a290433
jchen8000 <jchen8000@hotmail.com>
2021-04-08 15:44:24 -0400

    Add some more files to 6. for Face and Eye Detection.

--- 3. OpenCV Basics/ShowYoutube.py
+++ 3. OpenCV Basics/ShowYoutube.py
@@ -1,27 +1,39 @@
 #
-# Show a video from youtube link.
+# Show a youtube video
 # need to install package: pafy, youtube_dl
 #
 import cv2
 import pafy
 
-# Specify an Youtube video URL
-url = "https://www.youtube.com/watch?v=a3ICNMQW7Ok"
-video = pafy.new(url)
-best = video.getbest(preftype="mp4")
-cap = cv2.VideoCapture()
-cap.open(best.url)
+def list_streams(streams):
+    for i, stream in enumerate(streams):
+        print(i, stream)
 
-success, img = cap.read()
-while success:
-    cv2.imshow("Youtube", img)
-
-    # Press ESC key to break the loop
-    if cv2.waitKey(10) & 0xFF == 27:
-        break
+def show_video(stream, title):
+    cap = cv2.VideoCapture()
+    cap.open(stream.url)
     success, img = cap.read()
+    while success:
+        cv2.imshow(title, img)
+        # Press ESC key to break the loop
+        if cv2.waitKey(10) & 0xFF == 27:
+            break
+        success, img = cap.read()
+    cap.release()
+    cv2.destroyWindow(title)
 
-cap.release()
-cv2.destroyWindow("Youtube")
+if __name__ == "__main__":
+    # Specify an Youtube video URL
+    url = "https://www.youtube.com/watch?v=a3ICNMQW7Ok"
+    video = pafy.new(url)
 
+    # List the video streams
+    list_streams(video.videostreams)
 
+    # Show the best stream
+    best_stream = video.getbest(preftype="mp4")
+    show_video(best_stream, "Youtube Best Stream")
+
+    # Show a selected stream
+    stream_426_240 = video.videostreams[3]
+    show_video(stream_426_240, "Youtube Stream with 426x240")
---
+++ 6. Object Detection/FaceEyeDetection.py
@@ -0,0 +1,29 @@
+import cv2
+
+def detect_face_and_eye(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
+    print('Faces found: ', len(faces))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        face_gray = gray[y:y + h, x:x + w]
+        face_color = image[y:y + h, x:x + w]
+        eyes = eye_cascade.detectMultiScale(face_gray, scaleFactor=1.1, minNeighbors=3)
+        print('Eyes found: ', len(eyes))
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(face_color, (ex, ey), (ex + ew, ey + eh), (255, 0, 0), 2)
+    return image
+
+if __name__ == "__main__":
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +
+                                         "haarcascade_frontalface_default.xml")
+    eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +
+                                        "haarcascade_eye.xml")
+
+    img = cv2.imread("../res/pexels-bess-hamiti-35188.jpg")
+    cv2.imshow("Face and Eye Detection", detect_face_and_eye(img) )
+
+    cv2.waitKey(0)
+    cv2.destroyWindow("Face and Eye Detection")
+
+
