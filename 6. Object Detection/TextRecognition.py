58e31a25ee4221fffba17d5ec783b961c03136ef
James <jchen8000@hotmail.com>
2023-03-10 01:13:52 +0000

    update text recognition

--- 6. Object Detection/TextRecognition.py
+++ 6. Object Detection/TextRecognition.py
@@ -27,7 +27,7 @@ def recognize_by_char(image):
         char = char.split(" ")
         c,x,y,w,h = char[0], int(char[1]), int(char[2]), int(char[3]), int(char[4])
         cv2.rectangle(image, (x,height-y), (w,height-h), (0,0,255),1)
-        cv2.putText(image, c, (x,height-y-20), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0,0,255), 1)
+        cv2.putText(image, c, (x,height-y-35), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0,0,255), 1)
 
 def recognize_by_word(image):
     words = pytesseract.image_to_data(image)
@@ -38,12 +38,13 @@ def recognize_by_word(image):
             if len(word) == 12:
                 t,x,y,w,h = word[11], int(word[6]), int(word[7]), int(word[8]), int(word[9])
                 cv2.rectangle(image, (x,y), (w+x,h+y), (0,0,255),1)
-                cv2.putText(image, t, (x,y), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0,0,255), 1)
+                cv2.putText(image, t, (x,y-5), cv2.FONT_HERSHEY_COMPLEX, 0.9, (0,0,255), 1)
 
 def text_recognition():
     img = cv2.imread("../res/text_for_ocr.png")
     width, height = 880, 760
-    points = [(69, 1), (1089, 83), (1020, 947), (0, 866)]
+    # points = [(69, 1), (1089, 83), (1020, 947), (0, 866)]
+    points = [(64,106), (1058,182), (998,940), (4,863)]
     warped = perspective_warp(points, width, height, img)
 
     cv2.imshow("Warped", warped)
@@ -62,5 +63,8 @@ def text_recognition():
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
+    # #For Windows
+    # pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
+    #For Linux/Ubuntu
+    pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"
     text_recognition()
